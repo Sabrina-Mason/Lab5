758a96038cb6249fa5de0de0bf6b1e829f79f0f9
Tallon Simmons <tsims340@gmail.com>
2017-06-29 22:25:06 -0400

    first commit

---
+++ Lab5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            Console.WriteLine(rng.Next(1, 7));
+
+            //This is as far as i got, didnt understand the rest//
+   
+
+
+
+
+        }
+    }
+}
